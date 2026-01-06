6cc2cb2d6d8c289ebe922d5aebb2327f1759430a
404saugat404 <saugat556513@gmail.com>
2026-01-06 21:42:17 +0545

    basic websocket connection

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 print("successful running after uv")
 
@@ -9,3 +9,18 @@ app = FastAPI()
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print("🟢 WebSocket connected")
+
+    try:
+        while True:
+            message = await websocket.receive_text()
+            print(f"📩 Received: {message}")
+
+            await websocket.send_text(f"Echo: {message} , this is me")
+
+    except WebSocketDisconnect:
+        print("🔴 WebSocket disconnected")
